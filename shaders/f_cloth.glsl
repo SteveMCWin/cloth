173dc3be7d990bd95262b810afd3989bbfb86305
SteveMCWin <stevp003@gmail.com>
2025-01-16 19:22:57 +0100

    changed the ligthing just a bit

--- shaders/f_cloth.glsl
+++ shaders/f_cloth.glsl
@@ -10,19 +10,23 @@ uniform vec3 lightColor;
 uniform vec3 clothColor;
 
 void main() {
+    // first check if normal is pointing away from the view pos, if so, reverse the normal
     // ambient
     float ambientStrength = 0.3;
     vec3 ambient = ambientStrength * lightColor;
 
     // diffuse
     vec3 norm = normalize(vNormal);
+    vec3 viewDir = normalize(viewPos - vFragPos);
+
+    norm *= floor(dot(norm, viewDir)) * 2.0 + vec3(1.0); 
+
     vec3 lightDir = normalize(lightPos - vFragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
     // specular
     float specularStrength = 0.3;
-    vec3 viewDir = normalize(viewPos - vFragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * lightColor;
